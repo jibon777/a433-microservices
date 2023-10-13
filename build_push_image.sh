f4abcd5eec340d6187314649e35c110614035356
Your Name <el_emperatore@yaho.com>
2023-10-13 12:40:13 +0700

    add new test

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Perintah untuk membuat Docker image dari Dockerfile dengan nama item-app dan tag v1
 docker build -t item-app:v1 .
 
-# Melihat daftar image di lokal
 docker images
 
-# Mengubah nama image agar sesuai dengan format Docker Hub
 docker tag item-app:v1 jibon/item-app:v1
 
-# Login ke Docker Hub
 echo $PASSWORD_DOCKER_HUB | docker login -u jibon --password-stdin
 
-# Mengunggah image ke Docker Hub
 docker push jibon/item-app:v1
